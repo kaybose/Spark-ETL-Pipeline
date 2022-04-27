86dd0f13086904c4461ca4cf9799cbbe46de2b55
kaybose <99802300+kaybose@users.noreply.github.com>
2022-04-27 09:42:45 +0530

    Update load.py

--- load.py
+++ load.py
@@ -1,9 +1,10 @@
 
 def load_data(spark, df_transformed, tgt_dir, file_format):
-    print("Loading...")
+    #print("Loading...")
 
     spark.write. \
         format(file_format). \
         save(tgt_dir)
 
+    #sampleDF.createOrReplaceTempView("sampleStudentTable")
 
